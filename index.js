d82edd89a250eb752a1831a75bb91d0a2c550ff6
Dennis Kimathi <kimathidennis38@gmail.com>
2022-06-05 14:39:06 +0300

    commit tree 35693124345424214a8d596b8e0c153297780b3a

--- index.js
+++ index.js
@@ -1,1 +1,94 @@
+// // Your code here
+
+// function moveDodgerLeft() {
+
+//     document.addEventListener('keydown', function(event){
+//         console.log(event)
+//         if(event.key === 'ArrowLeft'){     
+//             var left = dodger.style.left.replace("px", "");
+//             var newPosition = parseInt(left, 10);
+
+//             if (newPosition > 0) {
+//                 dodger.style.left = `${newPosition - 10}px`}
+
+//                 console.log(newPosition)
+    
+//         } 
+        
+//     })
+
+// }
+
+// moveDodgerLeft();
+
+
+// function moveDodgerRight() {
+
+//     document.addEventListener('keydown', function(event){
+//         console.log(event)
+//         if(event.key === 'ArrowRight'){     
+//             var left = dodger.style.left.replace("px", "");
+//             var newPosition = parseInt(left, 10);
+
+//             if (newPosition < 360) {
+//                 dodger.style.left = `${newPosition + 10}px`}
+
+//                 console.log(newPosition)
+    
+//         } 
+        
+//     })
+
+// }
+
+// moveDodgerRight();
+
 // Your code here
+
+function moveDodgerLeft() {
+
+    document.addEventListener('keydown', function(event){
+        console.log(event)
+        if(event.key === 'ArrowLeft'){     
+            var left = dodger.style.left.replace("px", "");
+            left = parseInt(left);
+
+            moveDodgerLeft();
+
+            var newPosition = dodger.style.left
+            newPosition = parseInt(newPosition)
+
+            if (newPosition > 0) {
+                dodger.style.left = `${newPosition - 10}px`}
+
+                console.log(newPosition)
+    
+        } 
+        
+    })
+
+}
+
+moveDodgerLeft();
+
+
+// function moveDodgerRight() {
+
+//     document.addEventListener('keydown', function(event){
+//         console.log(event)
+//         if(event.key === 'ArrowRight'){     
+//             var left = dodger.style.left.replace("px", "");
+//             var newPosition = parseInt(left, 10);
+
+//             if (newPosition < 360) {
+//                 dodger.style.left = `${newPosition + 10}px`}
+
+//                 console.log(newPosition)
+    
+//         } 
+        
+//     })
+
+// }
+
+// moveDodgerRight();
